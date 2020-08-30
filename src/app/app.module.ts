7cfcd70ccc7d72312e7c2f8efd10550f10f84d75
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-08-31 01:33:54 +0300

    generate necessities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { ErrorpageComponent } from './components/errorpage/errorpage.component';
+import { DateAgoPipe } from './pipe/date-ago.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    RepoComponent,
+    NavbarComponent,
+    SearchComponent,
+    ErrorpageComponent,
+    DateAgoPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/gh-users.spec.ts
@@ -0,0 +1,7 @@
+import { GhUsers } from './gh-users';
+
+describe('GhUsers', () => {
+  it('should create an instance', () => {
+    expect(new GhUsers()).toBeTruthy();
+  });
+});
