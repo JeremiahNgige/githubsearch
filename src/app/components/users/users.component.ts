98f0f277c20890981311e0fd91d736e20d81e1db
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-08-31 02:17:35 +0300

    user search get component

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GhUsers }from '../../models/gh-users';
+import { GhRepo }from '../../models/gh-repo';
+import { GhServiceService } from '../../service/gh-service.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  searchUsers = true;
 
-  ngOnInit(): void {
+  githubUsers: GhUsers;
+  githubRepos: GhRepo;
+  userName: string;
+
+  constructor(private githubUserServive: GhServiceService, private githubRepoServive: GhServiceService) { }
+
+  search(userName) {
+    this.githubUserServive.getUser(userName).then((success) => {
+      this.githubUsers = this.githubUserServive.updateUsers;
+    },
+      (error) => {
+        alert("username not found");
+        console.log(error)
+      })
+
+    this.githubRepoServive.getRepo(userName).then((results) => {
+      this.githubRepos = this.githubRepoServive.updateRepo;
+    },
+      (error) => {
+        console.log(error);
+      })
+  }
+
+  ngOnInit() {
+    this.search('JeremiahNgige');
   }
 
 }
