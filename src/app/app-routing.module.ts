90b5c5e3f13c6a59df272c257af4b1cbf7d541f2
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-08-31 17:43:44 +0300

    fix search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UsersComponent } from './components/users/users.component';
-import { RepoComponent } from './components/repo/repo.component';
-import { ErrorpageComponent } from './components/errorpage/errorpage.component';
+
 
 const routes: Routes = [
-  { path: 'users', component: UsersComponent },
-  { path: 'repos', component: RepoComponent },
-  { path: '**', component: ErrorpageComponent },
-
-  { path: '', redirectTo: "/users", pathMatch: "full" },
 ];
 
 @NgModule({
