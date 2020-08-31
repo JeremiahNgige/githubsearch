4366a22843234151d635ecfbcedc30df8e1e46a4
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-08-31 03:38:58 +0300

    route pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { ErrorpageComponent } from './components/errorpage/errorpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'repos', component: RepoComponent },
+  { path: '**', component: ErrorpageComponent },
+
+  { path: '', redirectTo: "/users", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
